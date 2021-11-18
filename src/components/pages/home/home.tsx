eff878c95ec947fc5595e0ffb56e87e50e6000b7
weltraumm <weltraummm@gmail.com>
2021-11-18 23:33:25 +0300

    add search

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,11 +1,43 @@
-import "../pages.scss";
+import "./home.scss";
+import { useEffect, useState } from "react";
+// import { SearchBar } from "./searchBar/searchBar";
+import { Card } from "./gameCard";
+// import { Categories } from "./categories/categories";
+// import { NewGames } from "./newGames/newGames";
+import axios from "axios";
+// import { response } from "express";
 
-export const HomePage: React.FC = () => (
-  <>
-    <h1>Home page</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nemo accusantium voluptates. Ratione adipisci
-      esse nemo sit officia velit eveniet explicabo! Nulla quos pariatur omnis eum quia, ea adipisci ad!
-    </p>
-  </>
-);
+interface IGames {
+  title: string;
+}
+
+export const HomePage: React.FC = () => {
+  const [games, setGames] = useState<IGames[]>([]);
+
+  const getGames = () => {
+    axios.get("http://localhost:3000/games").then((response) => {
+      setGames(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getGames();
+  }, []);
+
+  const [value, setValue] = useState("");
+
+  const filteredGames = games.filter((game) => {
+    return game.title.toLowerCase().includes(value.toLowerCase());
+  });
+
+  return (
+    <div className="background">
+      <input type="text" placeholder="Search" onChange={(event) => setValue(event.target.value)} />
+      <div className="games">
+        {filteredGames.map((game) => {
+          return <p>{game.title}</p>;
+        })}
+      </div>
+    </div>
+  );
+};
